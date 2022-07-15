c2c04a89bf59ae68648d4e8d8781da55c0e2715f
Charles Averill <charlesaverill20@gmail.com>
2022-07-15 17:03:12 -0500

    Ch1 patches

---
+++ releases/patchgen.sh
@@ -0,0 +1,2 @@
+flips --create ../clean/ZELOOTMA.z64 zelda_ocarina_mq_dbg.z64oot_legendary_edition_ch1_mqdb.bps
+flips --create ../clean/oot_v1.z64 zelda_ocarina_mq_dbg.z64 oot_legendary_edition_ch1_v1.bps
